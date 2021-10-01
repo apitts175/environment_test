bb5309c8497c5a6372c513b6ed0b7d82bf7621d4
apitts175 <apitts175@gmail.com>
2021-09-30 21:13:43 -0500

    book collection changes

---
+++ book-assignment-apitts175-main (1)/book-assignment-apitts175-main/spec/unit/unit_spec.rb
@@ -0,0 +1,32 @@
+# location: spec/unit/unit_spec.rb
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(title: 'harry potter', author: 'jk rowling', price: 15, published_date: Date.parse('2019-7-8'))
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a date' do
+    subject.published_date = nil
+    expect(subject).not_to be_valid
+  end
+end
